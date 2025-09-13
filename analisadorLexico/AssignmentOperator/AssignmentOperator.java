97b914fe6e1af7afd6d2c69e77b93756c77cadcc
unknown <mscarvall@gmail.com>
2025-09-12 22:04:43 -0300

    add operador de atribuicao

---
+++ analisadorLexico/AssignmentOperator/AssignmentOperator.java
@@ -0,0 +1,25 @@
+import java.text.CharacterIterator;
+import analisadorLexico.AFD;
+import analisadorLexico.Token;
+
+public class AssignmentOperator extends AFD {
+
+    @Override
+    public Token evaluate(CharacterIterator code) {
+        char c = code.current();
+
+        if (c == '-') {
+            String op = "" + c;
+            code.next();
+
+            if (code.current() == '>') { 
+                op += '>';
+                code.next();
+
+                return new Token("OP_ATR", op);
+            }
+        }
+
+        return null;
+    }
+}
