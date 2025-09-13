37f81d7bb3a5c9c5e384ca6a0b8f4108ace531d5
unknown <mscarvall@gmail.com>
2025-09-12 22:07:26 -0300

    add chaves e parenteses

---
+++ analisadorLexico/Delimiters/Delimiters.java
@@ -0,0 +1,26 @@
+import java.text.CharacterIterator;
+import analisadorLexico.AFD;
+import analisadorLexico.Token;
+
+public class Delimiters extends AFD {
+
+    @Override
+    public Token evaluate(CharacterIterator code) {
+        switch (code.current()) {
+            case '{':
+                code.next();
+                return new Token("ABR_CHV", "{");
+            case '}':
+                code.next();
+                return new Token("FCH_CHV", "}");
+            case '(':
+                code.next();
+                return new Token("ABR_PAR", "(");
+            case ')':
+                code.next();
+                return new Token("FCH_PAR", ")");
+            default:
+                return null;
+        }
+    }
+}
