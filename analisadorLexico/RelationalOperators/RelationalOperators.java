aa67f6cab6dd7637da0698941504b5fe9ad5fcc4
unknown <mscarvall@gmail.com>
2025-09-14 14:29:58 -0300

    add afd operadores relacionais

---
+++ analisadorLexico/RelationalOperators/RelationalOperators.java
@@ -0,0 +1,66 @@
+package analisadorLexico.RelationalOperators;
+import java.text.CharacterIterator;
+import analisadorLexico.AFD;
+import analisadorLexico.Token;
+
+public class RelationalOperators extends AFD {
+
+    @Override
+    public Token evaluate(CharacterIterator code) {
+        StringBuilder word = new StringBuilder();
+
+        if (code.current() == '>') {
+            word.append('>');
+            code.next();
+
+            if (code.current() == '=') { 
+                word.append('=');
+                code.next();
+
+                // retorna >=
+                return new Token("OP_REL", word.toString());
+            } else {
+                // retorna >
+                return new Token("OP_REL", word.toString());
+            }
+            
+        }
+
+        if (code.current() == '<') {
+            word.append('<');
+            code.next();
+
+            if (code.current() == '>') { 
+                word.append('>');
+                code.next();
+
+                // retorna <>
+                return new Token("OP_REL", word.toString());
+
+            } else if (code.current() == '=') {
+                word.append('=');
+                code.next();
+                
+                // retorna <=
+                return new Token("OP_REL", word.toString());
+
+            } else if (code.current() == '-') { 
+                word.append('-');
+                code.next();
+
+                if (code.current() == '>') {
+                    word.append('>');
+                    code.next();
+                    
+                    // retorna <->
+                    return new Token("OP_REL", word.toString());
+                }
+            } else { 
+                // retorna <
+                return new Token("OP_REL", word.toString());
+            }
+        }
+
+        return null;
+    }
+}
