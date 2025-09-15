11dc3ef60c0e8fb31520e79ef9c97465659ffd55
unknown <mscarvall@gmail.com>
2025-09-15 20:13:54 -0300

    leitor de arquivo

--- analisadorLexico/Main.java
+++ analisadorLexico/Main.java
@@ -1,5 +1,8 @@
 package analisadorLexico;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
@@ -10,13 +13,35 @@ public class Main{
         System.out.println(op);
         System.out.println(num2);
      */ 
-        String code = "(;)+2.4+3+{Se}+<>->><->>=<=<#uai...bloco de comentarios...so#";
+        String code = "";
+        
+      
+        String nomeArquivo = "C:\\ProjetoCompilador\\analisadorLexico\\arquivo.txt"; // Substitua pelo caminho do seu arquivo
+        File arquivo = new File(nomeArquivo);
+        Scanner scanner = null;
+
+        try {
+            scanner = new Scanner(arquivo); 
+
+            while (scanner.hasNextLine()) { 
+                String linha = scanner.nextLine(); 
+                code += linha; 
+            }
+
+        } catch (FileNotFoundException e) {
+            System.err.println("Erro: O arquivo não foi encontrado.");
+            e.printStackTrace();
+        } finally {
+            if (scanner != null) {
+                scanner.close(); // Fecha o Scanner para liberar recursos
+            }
+        }
+
         Lexer lexer = new Lexer(code);
         List<Token> tokens = lexer.getTokens();
 
         for(Token t: tokens){
             System.out.println(t);
         }
-      
     }
 }
