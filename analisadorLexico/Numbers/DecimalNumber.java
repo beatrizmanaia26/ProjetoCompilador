2bffcf63b5c217ceb0178d0d379c841847774de3
carvalhosmari <mscarvall@gmail.com>
2025-10-09 22:01:56 -0300

    upd afd

--- analisadorLexico/Numbers/DecimalNumber.java
+++ analisadorLexico/Numbers/DecimalNumber.java
@@ -7,30 +7,6 @@ import analisadorLexico.Token;
 //esse inclui int e float (separar em 2)
 public class DecimalNumber extends AFD{
 
-    // @Override
-    // public Token evaluate(CharacterIterator code){
-    //     if(Character.isDigit(code.current())){
-    //         String number = readNumber(code);
-    //         if(code.current() == '.'){
-    //             number += '.';
-    //             code.next();
-    //             number += readNumber(code);
-    //         }
-    //         if (isTokenSeparator(code)){
-    //             return new Token("DECIMAL", number);
-    //         }
-    //     }
-    //     return null;
-    // }
-    // private String readNumber(CharacterIterator code){
-    //     String number = "";
-    //     while(Character.isDigit(code.current())){
-    //         number += code.current();
-    //         code.next();
-    //     }
-    //     return number;
-    // }
-
     @Override
     public Token evaluate(CharacterIterator code) {
         // se não começa com dígito, não é numero
